fc604c521f3159a4712e55ec4a596266a2238ed6
Hugo Wong <huang_chuojun@126.com>
2024-04-01 15:25:44 +0800

    初始化项目(umi)

---
+++ src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/hcj/Desktop/2024/yupi/code/user-center/second-time/frontend/node_modules/umi/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','layout','layoutActionRef','request',],
+});
+
+export { plugin };
---
+++ src/.umi/plugin-helmet/exports.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// @ts-ignore
+export { Helmet } from '/Users/hcj/Desktop/2024/yupi/code/user-center/second-time/frontend/node_modules/react-helmet';
